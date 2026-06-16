95a445774d31421d8a7a16560ccf411a5fb7f70f
anurag sharma <anuragsharma2092003@gmail.com>
2026-06-16 09:11:13 +0530

    (Anurag)level1:commit all question

---
+++ core-java-practise/gcr-codebase/programming-elements/level1/AverageMarks.java
@@ -0,0 +1,12 @@
+package core-java-practise.gcr-codebase.programming-elements.level1;
+
+public class AverageMarks {
+    public static void main(String[] args){
+        int m=95;
+        int e=23;
+        int h=88;
+        int avg=(m+e+h)/2;
+        System.out.println(avg);
+    }
+    
+}
